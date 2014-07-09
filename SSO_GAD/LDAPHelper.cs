a547683427d59555f576c08289bdb1c2f2f81ac3
Jose Mario Urquia <mariourquia@outlook.com>
2014-07-09 00:05:59 -0600

    busca contrasena en OpenLDAP

---
+++ SSO_GAD/LDAPHelper.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.DirectoryServices.Protocols;
+using System.Globalization;
+using System.Net;
+using System.Security;
+
+
+namespace SSO_GAD
+{
+    public class LDAPHelper
+    {
+        private readonly LdapConnection ldapConnection;
+        private readonly string searchBaseDN;
+        private readonly int pageSize;
+
+        public LDAPHelper(
+            string searchBaseDN,
+            string hostName,
+            int portNumber,
+            AuthType authType,
+            string connectionAccountName,
+            SecureString connectionAccountPassword,
+            int pageSize)
+        {
+
+            var ldapDirectoryIdentifier = new LdapDirectoryIdentifier(
+                hostName,
+                portNumber,
+                true,
+                false);
+
+            var networkCredential = new NetworkCredential(
+                connectionAccountName,
+                connectionAccountPassword);
+
+            ldapConnection = new LdapConnection(
+                ldapDirectoryIdentifier,
+                networkCredential) { AuthType = authType };
+
+            ldapConnection.SessionOptions.ProtocolVersion = 3;
+
+            this.searchBaseDN = searchBaseDN;
+            this.pageSize = pageSize;
+        }
+
+        public IEnumerable<SearchResultEntryCollection> PagedSearch(
+            string searchFilter,
+            string[] attributesToLoad)
+        {
+
+            var pagedResults = new List<SearchResultEntryCollection>();
+
+            var searchRequest = new SearchRequest
+                    (searchBaseDN,
+                     searchFilter,
+                     SearchScope.Subtree,
+                     attributesToLoad);
+
+
+            var searchOptions = new SearchOptionsControl(SearchOption.DomainScope);
+            searchRequest.Controls.Add(searchOptions);
+
+            var pageResultRequestControl = new PageResultRequestControl(pageSize);
+            searchRequest.Controls.Add(pageResultRequestControl);
+
+            while (true)
+            {
+                var searchResponse = (SearchResponse)ldapConnection.SendRequest(searchRequest);
+                var pageResponse = (PageResultResponseControl)searchResponse.Controls[0];
+
+                yield return searchResponse.Entries;
+                if (pageResponse.Cookie.Length == 0)
+                    break;
+
+                pageResultRequestControl.Cookie = pageResponse.Cookie;
+            }
+
+
+        }
+    }
+}
--- SSO_GAD/welcome.aspx.cs
+++ SSO_GAD/welcome.aspx.cs
@@ -11,8 +11,8 @@ using System.Net;
 using System.Text;
 using System.IO;
 using System.Security.Cryptography;
-
-
+//using System.DirectoryServices.Protocols;
+using System.DirectoryServices;
 
 
 namespace SSO_GAD
@@ -20,15 +20,9 @@ namespace SSO_GAD
     public partial class welcome : System.Web.UI.Page
     {
 
-        // Fields
-        //protected HtmlForm form1;
-        //protected HyperLink HyperLink1;
-        //protected Image Image1;
-        //protected Label Label1;
+
         public static string Pass;
-        //protected TextBox TextBox1;
         public static string Username;
-        private String claveCifrado="Glt2014web";
 
         // Methods
         public bool AcceptAllCertifications(object sender, X509Certificate certification, X509Chain chain, SslPolicyErrors sslPolicyErrors)
@@ -61,7 +55,7 @@ namespace SSO_GAD
             return container;
         }
 
-        private void metodo1(string url)
+        private void metodoPost(string url)
         {
             Uri uri = new Uri(url);
             ASCIIEncoding encoding = new ASCIIEncoding();
@@ -85,20 +79,22 @@ namespace SSO_GAD
             HttpWebResponse response = (HttpWebResponse)request.GetResponse();
             StreamReader reader = new StreamReader(response.GetResponseStream());
             this.TextBox1.Text = this.TextBox1.Text + "ok";
-            string str2 = "";
+            string cookie = "";
             if (response.Cookies.Count > 0)
             {
-                foreach (Cookie cookie in response.Cookies)
+                foreach (Cookie c in response.Cookies)
                 {
-                    str2 = cookie.Value;
-                    this.transferCookieToApplication(cookie, uri.Host);
+                    cookie = c.Value;
+                    this.transferCookieToApplication(c, uri.Host);
                 }
             }
             this.TextBox1.Text = this.TextBox1.Text + "\nRecibiendo respuesta  del Sitio \n comienzo...";
             this.TextBox1.Text = this.TextBox1.Text + reader.ReadToEnd();
             this.TextBox1.Text = this.TextBox1.Text + "\n  fin.";
             this.TextBox1.Text = this.TextBox1.Text + "\nEstableciendo URL...";
-            str2 = "https://s361717226.onlinehome.us:8080/webclient/WebClient.jsf;jsessionid=" + str2;
+            
+            String str2 = String.Format("{0};jsessionid={1}", Properties.Settings.Default.WebClient_URL_SSO,cookie);
+           
             this.TextBox1.Text = this.TextBox1.Text + "ok\n";
             this.HyperLink1.NavigateUrl = str2;
             this.TextBox1.Text = this.TextBox1.Text + "\nCerrando conexiones...";
@@ -106,54 +102,52 @@ namespace SSO_GAD
             response.Close();
             this.TextBox1.Text = this.TextBox1.Text + "ok";
             this.TextBox1.Text = this.TextBox1.Text + "\n\nFavor utilice el URL para ir al GOANY. ";
-            Response.Redirect(str2);
+            //Response.Redirect(str2);
         }
 
-        private void metodo2(string url)
-        {
-            ASCIIEncoding encoding = new ASCIIEncoding();
-            string s = "username=" + Username + "&password=" + Pass;
-            byte[] bytes = encoding.GetBytes(s);
-            this.Label1.Text = "/" + s + "/";
-            WebRequest request = WebRequest.Create(url);
-            request.Method = "POST";
-            request.ContentType = "application/x-www-form-urlencoded";
-            request.ContentLength = s.Length;
-            Stream requestStream = request.GetRequestStream();
-            requestStream.Write(bytes, 0, bytes.Length);
-            requestStream.Close();
-            requestStream = request.GetResponse().GetResponseStream();
-            StreamReader reader = new StreamReader(requestStream);
-            this.TextBox1.Text = reader.ReadToEnd();
-            reader.Close();
-            requestStream.Close();
-        }
 
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!base.IsPostBack)
             {
                 Username = this.Session["user"].ToString().Trim();
-                //Pass = this.Session["pass"].ToString().Trim();
-                Pass = descifrar(Properties.Settings.Default.GADPass);
+                Pass = RetrievePassFromLDAP(Username);
                 this.Label1.Text = "Bienvenido " + Username;
             }
             try
             {
 
-                /*//comienzo de cifrado
-                String passCifrada = cifrar(Properties.Settings.Default.GADPass);
-                
-                //fin de cifrado*/
-                string url = "https://s361717226.onlinehome.us:8080/login";
+                string url = Properties.Settings.Default.WebClient_URL;
                 ServicePointManager.ServerCertificateValidationCallback = new RemoteCertificateValidationCallback(this.AcceptAllCertifications);                             
-                this.metodo1(url);
+                this.metodoPost(url);
             }
             catch (Exception exception)
             {
                 this.TextBox1.Text = "\t\t*****ERROR****\n  " + exception.ToString();
             }
         }
+        private String RetrievePassFromLDAP(String user)
+        {
+
+            String path = String.Format("LDAP://{0}:{1}/{2}",
+                Properties.Settings.Default.LDAP_Host,
+                Properties.Settings.Default.LDAP_Port,
+                Properties.Settings.Default.LDAP_DNBase);
+            
+            DirectoryEntry nRoot = new DirectoryEntry(path);
+            nRoot.AuthenticationType = AuthenticationTypes.Anonymous;
+            //nRoot.Username = "cn=Manager,dc=maxcrc,dc=com";
+            //nRoot.Password = "secret";
+            DirectorySearcher nDS = new DirectorySearcher(nRoot);
+            nDS.SearchScope = SearchScope.Subtree;
+            nDS.Filter = String.Format("uid={0}",Username);
+            nDS.PropertiesToLoad.Add(Properties.Settings.Default.LDAP_AttributePass);
+            SearchResult sr = nDS.FindOne();
+            String resp = sr.Properties[Properties.Settings.Default.LDAP_AttributePass][0].ToString();                       
+            return resp;
+
+        }
+       
 
         protected internal void transferCookieToApplication(Cookie c, string host)
         {
@@ -166,56 +160,7 @@ namespace SSO_GAD
             };
             HttpContext.Current.Response.Cookies.Add(cookie);
         }
-
-        public string cifrar(string cadena)
-        {
-
-            byte[] llave; //Arreglo donde guardaremos la llave para el cifrado 3DES.
-
-            byte[] arreglo = UTF8Encoding.UTF8.GetBytes(cadena); //Arreglo donde guardaremos la cadena descifrada.
-
-            // Ciframos utilizando el Algoritmo MD5.
-            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
-            llave = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(claveCifrado));
-            md5.Clear();
-
-            //Ciframos utilizando el Algoritmo 3DES.
-            TripleDESCryptoServiceProvider tripledes = new TripleDESCryptoServiceProvider();
-            tripledes.Key = llave;
-            tripledes.Mode = CipherMode.ECB;
-            tripledes.Padding = PaddingMode.PKCS7;
-            ICryptoTransform convertir = tripledes.CreateEncryptor(); // Iniciamos la conversión de la cadena
-            byte[] resultado = convertir.TransformFinalBlock(arreglo, 0, arreglo.Length); //Arreglo de bytes donde guardaremos la cadena cifrada.
-            tripledes.Clear();
-
-            return Convert.ToBase64String(resultado, 0, resultado.Length); // Convertimos la cadena y la regresamos.
-        }
-        public string descifrar(string cadena)
-        {
-
-            byte[] llave;
-
-            byte[] arreglo = Convert.FromBase64String(cadena); // Arreglo donde guardaremos la cadena descovertida.
-
-            // Ciframos utilizando el Algoritmo MD5.
-            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
-            llave = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(claveCifrado));
-            md5.Clear();
-
-            //Ciframos utilizando el Algoritmo 3DES.
-            TripleDESCryptoServiceProvider tripledes = new TripleDESCryptoServiceProvider();
-            tripledes.Key = llave;
-            tripledes.Mode = CipherMode.ECB;
-            tripledes.Padding = PaddingMode.PKCS7;
-            ICryptoTransform convertir = tripledes.CreateDecryptor();
-            byte[] resultado = convertir.TransformFinalBlock(arreglo, 0, arreglo.Length);
-            tripledes.Clear();
-
-            string cadena_descifrada = UTF8Encoding.UTF8.GetString(resultado); // Obtenemos la cadena
-            return cadena_descifrada; // Devolvemos la cadena
-        }
-
-
+        
 
 
 
